f44c7512a69bd615a157dc3e94dd24d3eda7cca0
105mm <king7532@knou.ac.kr>
2024-03-10 20:54:43 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+//자바의 코드는 class 안, main 메소드 안에서 작성한다.
+//자바의 모든 명령어가 끝나면 반드시 세미콜론을 붙여준다.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        //안녕하세요 이것은 주석입니다.
+        //주석은 코드를 해석할 때 생략한다.
+        System.out.println("Hello world!");
+        System.out.println("안녕하세요");
+        System.out.println("123");
+        System.out.print("ㅎ2");System.out.print("ㅎ2");
     }
 }
