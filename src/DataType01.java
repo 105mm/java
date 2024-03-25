6260b319d71375e2373eb526043507ddc9dda196
105mm <king7532@knou.ac.kr>
2024-03-25 21:12:33 +0900

    commit

---
+++ src/DataType01.java
@@ -0,0 +1,16 @@
+public class DataType01 {
+    public static void main(String[] args) {  //psvm
+        int num;
+
+        byte n1 = 127; // -128 ~ 127
+        short n2 = 30000; // -32768 ~ 32767
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = 10; // 오류
+
+        char c1 = '가';
+        char c2 = "가"; // 오류
+        String c3 = "자바"; //
+    }
+}
